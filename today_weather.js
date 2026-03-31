4ff21c98dc7ececdab27d389f089cbe6f23fee6b
PUynn <Hoangthuongcute2005@gmail.com>
2026-03-31 16:23:30 +0700

    Add Today's weather

---
+++ today_weather.js
@@ -0,0 +1,7 @@
+const temperature = 28; 
+const conditions = "sunny and super hot";
+const sunriseTime = "06:10 AM";
+const sunsetTime = "06:45 PM";
+
+const weatherSummary = `Today the temperature is  ${temperature}°C, and the weather is ${conditions}. The sun rises at ${sunriseTime} and sets at ${sunsetTime}.`;
+console.log(weatherSummary);
