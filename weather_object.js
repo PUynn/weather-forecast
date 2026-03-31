617690fce10aaa6e13f25b9a5083073fc5ee1521
PUynn <Hoangthuongcute2005@gmail.com>
2026-03-31 16:24:59 +0700

    Add Weather Object

---
+++ weather_object.js
@@ -0,0 +1,7 @@
+const currentWeather = {
+  temperature: 80,
+  conditions: 'super hot'
+};
+
+console.log(`Current temperature: ${currentWeather.temperature}`);
+console.log(`It's ${currentWeather.temperature} degrees F and ${currentWeather.conditions}.`);
